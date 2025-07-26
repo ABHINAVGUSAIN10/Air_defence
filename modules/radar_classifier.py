c726e35a1bd0cbff92bdb995fee7c31b0cb667cc
ABHINAVGUSAIN10 <160138729+ABHINAVGUSAIN10@users.noreply.github.com>
2025-07-26 18:09:41 +0530

    Update radar_classifier.py

--- modules/radar_classifier.py
+++ modules/radar_classifier.py
@@ -1,14 +1,14 @@
 import torch
 import numpy as np
-from models.radar_cnn import RadarCNN # Assumes models folder is accessible
+from models.radar_cnn import RadarCNN 
 
-# --- IMPORTANT: Update these paths to your model and class files ---
+
 MODEL_PATH = r'C:\Users\Abhinav Gusain\Documents\Air_defence\models\best_radar_cnn.pth'
 CLASSES_PATH = r'C:\Users\Abhinav Gusain\Documents\Air_defence\data\radar_signal\classes-fixed.txt'
 
-# Load the model and class labels once when the application starts
+
 model = RadarCNN()
-# Add weights_only=True to fix the warning
+
 model.load_state_dict(torch.load(MODEL_PATH, map_location=torch.device('cpu'), weights_only=True))
 model.eval()
 
@@ -16,15 +16,7 @@ with open(CLASSES_PATH) as f:
     classes = [line.strip() for line in f]
 
 def classify_modulation(iq_array: np.ndarray):
-    """
-    Takes a raw I/Q numpy array and returns the predicted modulation class.
-    
-    Args:
-        iq_array (np.ndarray): The I/Q data of shape (2, 1024).
-        
-    Returns:
-        str: The name of the predicted modulation type or an error message.
-    """
+   
     if not isinstance(iq_array, np.ndarray):
         return "I/Q data not provided"
 
