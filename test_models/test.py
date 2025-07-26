724c9d27f9117bf1cdb4127524238b937ae36db6
ABHINAVGUSAIN10 <160138729+ABHINAVGUSAIN10@users.noreply.github.com>
2025-07-26 18:05:53 +0530

    Update test.py

--- test_models/test.py
+++ test_models/test.py
@@ -2,20 +2,20 @@ from ultralytics import YOLO
 from PIL import Image
 import matplotlib.pyplot as plt
 
-# Load the trained model
+
 model = YOLO(r"C:\Users\Abhinav Gusain\Documents\Air_defence\results\detection_outputs\weights\best.pt")
 
-# Load the image
+
 image_path = r"C:\Users\Abhinav Gusain\Documents\Air_defence\test_images\ShCH39Q.jpg"
 results = model(image_path)
 
-# Plot results
-results[0].show()  # To visualize bounding boxes
+
+results[0].show()  
 plt.imshow(results[0].plot())
 plt.axis("off")
 plt.show()
 
-# Print predicted classes
+
 for r in results:
     print("Classes detected:", r.names)
     print("Predicted classes:", r.boxes.cls)
