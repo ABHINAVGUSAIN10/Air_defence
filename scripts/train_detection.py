b7a58c59fdbd54c4ab9e57038385ec433d5dd518
ABHINAVGUSAIN10 <160138729+ABHINAVGUSAIN10@users.noreply.github.com>
2025-07-26 18:07:55 +0530

    Update train_detection.py

--- scripts/train_detection.py
+++ scripts/train_detection.py
@@ -2,23 +2,23 @@ from ultralytics import YOLO
 import os
 
 def main():
-    model = YOLO("yolov8s.pt")  # You can change to yolov8s.pt if VRAM allows
+    model = YOLO("yolov8s.pt")  
 
     model.train(
-        data=r"C:\Users\Abhinav Gusain\Documents\Air_defence\data\detection\data.yaml",  # path to your data YAML
+        data=r"C:\Users\Abhinav Gusain\Documents\Air_defence\data\detection\data.yaml",  
         epochs=40,
         imgsz=640,
         batch=16,
-        device=0,  # Set to 0 for GPU
+        device=0,  
         project=r"C:\Users\Abhinav Gusain\Documents\Air_defence\results\detection_outputs",
-        name="weights",  # Weights will be saved to results/weights
+        name="weights", 
         save=True,
-        save_period=5,  # Save every 5 epochs
+        save_period=5,  
         plots=True,
-        workers=0  # Needed for Windows sometimes, or set >0 if needed
+        workers=0  
     )
 
 if __name__ == "__main__":
     import multiprocessing
-    multiprocessing.freeze_support()  # Safe for Windows
+    multiprocessing.freeze_support()  
     main()
